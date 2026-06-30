f55bc312bd86b877432a0b9ab3addb38b259893e
michaelbag8 <michaelbag8@gmail.com>
2026-06-30 11:32:54 +0100

    import path corrected

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,135 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
-    //"election-voting-system/internal/handlers"
+	"election-voting-system/internal/handlers"
     "election-voting-system/internal/models"
     //"election-voting-system/internal/routes"
     //"election-voting-system/internal/services"
+   
 )
-
-type Election struct {
-	Voters     []models.Voter     `json:"voters"`
-	Candidates []models.Candidate `json:"candidates"`
-}
-type CreateVoterRequest struct {
-	Name    string `json:"name"`
-	VoterID string `json:"voterId"`
-}
-
-var election = Election{}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Election Voting System API is running")
-}
-
-func getVoters(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(election.Voters)
-	if err != nil {
-		http.Error(w, "encoding to json failed", http.StatusInternalServerError)
-		return
-	}
-}
-
-func getCandidates(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(election.Candidates)
-	if err != nil {
-		http.Error(w, "encoding to json failed", http.StatusInternalServerError)
-		return
-	}
-}
-
-func createCandidate(w http.ResponseWriter, r *http.Request){
-    http.Error(w, "Not implemented", http.StatusNotImplemented)
-}
-
-func createVoters(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var req CreateVoterRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	//checking for empty input
-	if strings.TrimSpace(req.Name) == "" || strings.TrimSpace(req.VoterID) == "" {
-		http.Error(w, "name and id cannot be empty", http.StatusBadRequest)
-		return
-	}
-
-	voter := models.Voter{
-		ID:       len(election.Voters) + 1,
-		Name:     req.Name,
-		VoterID:  req.VoterID,
-		HasVoted: false,
-	}
-	//checking for duplicate
-	for _, existingvoter := range election.Voters {
-		if existingvoter.VoterID == req.VoterID {
-			http.Error(w, "Voter ID already exists", http.StatusConflict)
-			return
-		}
-	}
-	//adding voter of non duplicate ID
-	election.Voters = append(election.Voters, voter)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-
-	if err := json.NewEncoder(w).Encode(voter); err != nil {
-		http.Error(w, "failed to encode", http.StatusInternalServerError)
-		return
-	}
-}
-
-//request dispatching
-func votersHandler(w http.ResponseWriter, r *http.Request){
-    switch r.Method{
-    case http.MethodGet:
-        getVoters(w,r)
-    case http.MethodPost:
-        createVoters(w,r)
-    default:
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-    }
-}
-
-func candidatesHandler(w http.ResponseWriter, r *http.Request){
-    switch r.Method{
-    case http.MethodGet:
-        getCandidates(w,r)
-    case http.MethodPost:
-        createCandidate(w,r)
-    default:
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-    }
-}
 func main() {
-	election.Voters = append(election.Voters,
+	handlers.Elections.Voters = append(handlers.Elections.Voters,
 		models.Voter{ID: 1, Name: "Michael", VoterID: "V001", HasVoted: false},
 		models.Voter{ID: 2, Name: "Sarah", VoterID: "V002", HasVoted: false},
 	)
 
-	election.Candidates = append(election.Candidates,
+	handlers.Elections.Candidates = append(handlers.Elections.Candidates,
 		models.Candidate{ID: 1, Name: "James", Party: "PDP"},
 		models.Candidate{ID: 2, Name: "Joel", Party: "APC"},
 		models.Candidate{ID: 3, Name: "Osyter", Party: "PNP"},
 	)
 
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/voters", votersHandler)
-    http.HandleFunc("/candidates", candidatesHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/voters", handlers.VotersHandler)
+    http.HandleFunc("/candidates", handlers.CandidatesHandler)
 
 	fmt.Println("Server running on :8080")
 	err := http.ListenAndServe(":8080", nil)
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,116 @@
 package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+    //"election-voting-system/internal/handlers"
+    "election-voting-system/internal/models"
+    //"election-voting-system/internal/routes"
+    //"election-voting-system/internal/services"
+)
+
+type Election struct {
+	Voters     []models.Voter     `json:"voters"`
+	Candidates []models.Candidate `json:"candidates"`
+}
+type CreateVoterRequest struct {
+	Name    string `json:"name"`
+	VoterID string `json:"voterId"`
+}
+
+var Elections = Election{}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Election Voting System API is running")
+}
+
+func getVoters(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(Elections.Voters)
+	if err != nil {
+		http.Error(w, "encoding to json failed", http.StatusInternalServerError)
+		return
+	}
+}
+
+func getCandidates(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(Elections.Candidates)
+	if err != nil {
+		http.Error(w, "encoding to json failed", http.StatusInternalServerError)
+		return
+	}
+}
+
+func createCandidate(w http.ResponseWriter, r *http.Request){
+    http.Error(w, "Not implemented", http.StatusNotImplemented)
+}
+
+func createVoters(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req CreateVoterRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	//checking for empty input
+	if strings.TrimSpace(req.Name) == "" || strings.TrimSpace(req.VoterID) == "" {
+		http.Error(w, "name and id cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	voter := models.Voter{
+		ID:       len(Elections.Voters) + 1,
+		Name:     req.Name,
+		VoterID:  req.VoterID,
+		HasVoted: false,
+	}
+	//checking for duplicate
+	for _, existingvoter := range Elections.Voters {
+		if existingvoter.VoterID == req.VoterID {
+			http.Error(w, "Voter ID already exists", http.StatusConflict)
+			return
+		}
+	}
+	//adding voter of non duplicate ID
+	Elections.Voters = append(Elections.Voters, voter)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(voter); err != nil {
+		http.Error(w, "failed to encode", http.StatusInternalServerError)
+		return
+	}
+}
+
+//request dispatching
+func VotersHandler(w http.ResponseWriter, r *http.Request){
+    switch r.Method{
+    case http.MethodGet:
+        getVoters(w,r)
+    case http.MethodPost:
+        createVoters(w,r)
+    default:
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+    }
+}
+
+func CandidatesHandler(w http.ResponseWriter, r *http.Request){
+    switch r.Method{
+    case http.MethodGet:
+        getCandidates(w,r)
+    case http.MethodPost:
+        createCandidate(w,r)
+    default:
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+    }
+}
